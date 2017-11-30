df95a948041c656b647c158676cab560d4271868
prtx <prtx.pratikshrestha@gmail.com>
2017-11-30 22:52:55 +0545

    Add sorting options for leaderboard

--- ccs.py
+++ ccs.py
@@ -14,6 +14,12 @@ BOLD   = "\033[;1m"
 def parse_args(args=None):
     parser = argparse.ArgumentParser(description="Market statistics for cryptocurrencies.")
     parser.add_argument("coin_id", type=str, nargs='?', help="Provide coin name for individual coin stats.")
+    
+    sort_group = parser.add_mutually_exclusive_group()
+    sort_group.add_argument("-p", "--price-sort", action="store_true", help="Sort by Price(USD)")
+    sort_group.add_argument("-hr", "--hourly-sort", action="store_true", help="Sort by Hourly %% Change")
+    sort_group.add_argument("-day", "--daily_sort", action="store_true", help="Sort by Daily %% Change")
+    sort_group.add_argument("-wk", "--weekly-sort", action="store_true", help="Sort by Weekly %% Change")
 
     if args:
         return parser.parse_args(args)
@@ -34,26 +40,30 @@ def check(request):
         exit()
 
 
-def leaderboard():
+def leaderboard(sort_key=None):
     url = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
     request = requests.get(url)
     check(request)    
    
     header = (BOLD, 'Name', 'Symbol', 'Price(USD)', '1h % Chg', '1d % Chg', '7d % Chg', NORMAL,)
+    raw_str = '| %-20s | %-6s | %10.2f | %s%8.2f%s | %s%8.2f%s | %s%8.2f%s |'
     print('%s| %-20s | %-6s | %-10s | %-8s | %-8s | %-8s |%s' % header)
     
     content = request.content.decode()
+    data = []
     for i in json.loads(str(content)):
-        data = (
+        data.append((
             i['name'],
             i['symbol'],
             float(i['price_usd']),
             *color_value(float(i['percent_change_1h'])),
             *color_value(float(i['percent_change_24h'])),
             *color_value(float(i['percent_change_7d'])),
-        )
-        raw = '| %-20s | %-6s | %10.2f | %s%8.2f%s | %s%8.2f%s | %s%8.2f%s |'
-        print(raw % data)
+        ))
+    
+    if sort_key: data.sort(key=lambda x: x[sort_key], reverse=True)
+    for row in data:
+        print(raw_str % row)
 
 
 def individual_stats(coin_id):
@@ -80,7 +90,12 @@ def main(args):
     if args.coin_id:
         individual_stats(args.coin_id)
     else:
-        leaderboard()
+        sort_key = None
+        if args.price_sort:  sort_key = 2
+        if args.hourly_sort: sort_key = 4
+        if args.daily_sort:  sort_key = 7
+        if args.weekly_sort: sort_key = 10
+        leaderboard(sort_key)
 
 
 if __name__ == "__main__":
